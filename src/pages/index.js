6589907a99860fd50326d1fd8a2fb9cf7dc62264
Arishwin <arishwinkishaan3@gmail.com>
2021-12-27 00:29:22 +0800

    rename portfolio

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,9 @@ export default () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{title || 'Gatsby Simplefolio'}</title>
+        <title>{title || "Arishwin's Portfolio"}</title>
         <html lang={lang || 'en'} />
-        <meta name="description" content={description || 'Gatsby Simplefolio'} />
+        <meta name="description" content={description || "Arishwin's Portfolio"} />
       </Helmet>
       <App />
     </>
