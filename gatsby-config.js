a6d56e0064272d9e70e0443021aefe9076464cff
Arishwin <arishwinkishaan3@gmail.com>
2021-12-27 00:43:28 +0800

    change theme colour of app

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         short_name: `Portfolio`,
         start_url: `/`,
         background_color: `#fff`,
-        theme_color: `#02aab0`,
+        theme_color: `#16bffd`,
         display: `standalone`,
         icon: 'src/images/favicon.png',
       },
